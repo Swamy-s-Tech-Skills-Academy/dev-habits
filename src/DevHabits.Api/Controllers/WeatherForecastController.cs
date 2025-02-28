cbcd096542190396be10ddfc618d94d23750d185
Viswanatha Swamy <vishipayyallore@yahoo.com>
2025-02-28 10:30:24 +0530

    Swamy/27feb work (#1)
    * Initial Version
    
    * Create DevHabits.sln
    
    * Update DevHabits.sln
    
    * Initial Version
    
    * Update WeatherForecastController.cs
    
    * Update launchSettings.json
    
    * Changes
    
    * Changes
    
    * Changes
    
    * Changes
    
    * Changes
    
    * Changes
    
    * Changes
    
    * Update DevHabits.sln

---
+++ src/DevHabits.Api/Controllers/WeatherForecastController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography;
+
+namespace DevHabits.Api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class WeatherForecastController(ILogger<WeatherForecastController> logger) : ControllerBase
+{
+    private static readonly string[] Summaries =
+    [
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    ];
+
+    private readonly ILogger<WeatherForecastController> _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+
+    [HttpGet(Name = "GetWeatherForecast")]
+    public IEnumerable<WeatherForecast> Get()
+    {
+
+        _logger.LogInformation("Getting weather forecast");
+
+        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        {
+            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            TemperatureC = RandomNumberGenerator.GetInt32(-20, 55),
+            Summary = Summaries[RandomNumberGenerator.GetInt32(Summaries.Length)]
+        })
+        .ToArray();
+    }
+}
