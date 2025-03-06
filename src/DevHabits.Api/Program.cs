80e2a590ad8c62eca3518b325c3f2d8e10d9602c
Viswanatha Swamy <vishipayyallore@yahoo.com>
2025-03-06 22:04:39 +0530

    Db Context

--- src/DevHabits.Api/Program.cs
+++ src/DevHabits.Api/Program.cs
@@ -1,3 +1,6 @@
+using DevHabits.Api.Database;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Migrations;
 using OpenTelemetry;
 using OpenTelemetry.Metrics;
 using OpenTelemetry.Resources;
@@ -10,6 +13,12 @@ WebApplicationBuilder? builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 
 builder.Services.AddOpenApi();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseNpgsql(
+                builder.Configuration.GetConnectionString("Database"),
+                npgsqlOptions => npgsqlOptions.MigrationsHistoryTable(HistoryRepository.DefaultTableName, Schemas.Application))
+            .UseSnakeCaseNamingConvention());
 
 builder.Services.AddOpenTelemetry()
     .ConfigureResource(resource => resource.AddService(builder.Environment.ApplicationName))
