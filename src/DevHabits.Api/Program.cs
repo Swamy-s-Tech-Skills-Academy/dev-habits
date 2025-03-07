fc6be141be43eacb2ccac99e90035dc98ef74c5e
Viswanatha Swamy <vishipayyallore@yahoo.com>
2025-03-07 15:45:53 +0530

    Swamy/06mar work (#6)
    * Update Directory.Packages.props
    
    * Initial Version
    
    * Update HabitTagConfiguration.cs
    
    * Db Context
    
    * Extension Method
    
    * Migrations
    
    * Migrations
    
    * Update 20250306165838_Add_Habits.cs

--- src/DevHabits.Api/Program.cs
+++ src/DevHabits.Api/Program.cs
@@ -1,3 +1,7 @@
+using DevHabits.Api.Database;
+using DevHabits.Api.Extensions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Migrations;
 using OpenTelemetry;
 using OpenTelemetry.Metrics;
 using OpenTelemetry.Resources;
@@ -10,6 +14,12 @@ WebApplicationBuilder? builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 
 builder.Services.AddOpenApi();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseNpgsql(
+                builder.Configuration.GetConnectionString("Database"),
+                npgsqlOptions => npgsqlOptions.MigrationsHistoryTable(HistoryRepository.DefaultTableName, Schemas.Application))
+            .UseSnakeCaseNamingConvention());
 
 builder.Services.AddOpenTelemetry()
     .ConfigureResource(resource => resource.AddService(builder.Environment.ApplicationName))
@@ -33,6 +43,8 @@ WebApplication? app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+
+    await app.ApplyMigrationsAsync();
 }
 
 app.UseHttpsRedirection();
