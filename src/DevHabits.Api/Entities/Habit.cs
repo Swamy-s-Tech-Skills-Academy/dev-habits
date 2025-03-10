3fc633d6630d1c3e178789bf26a8cc277c976372
Viswanatha Swamy <vishipayyallore@yahoo.com>
2025-03-10 13:28:50 +0530

    Swamy/07mar work (#7)
    * Update Directory.Packages.props
    
    * Update Habit.cs
    
    * Code formatting
    
    * Added Latest

--- src/DevHabits.Api/Entities/Habit.cs
+++ src/DevHabits.Api/Entities/Habit.cs
@@ -3,20 +3,33 @@
 public sealed class Habit
 {
     public string Id { get; set; }
+
     public string Name { get; set; } = string.Empty;
+
     public string? Description { get; set; }
+
     public HabitType Type { get; set; }
+
     public Frequency Frequency { get; set; }
+
     public Target Target { get; set; }
+
     public HabitStatus Status { get; set; }
+
     public bool IsArchived { get; set; }
+
     public DateOnly? EndDate { get; set; }
+
     public Milestone? Milestone { get; set; }
+
     public DateTime CreatedAtUtc { get; set; }
+
     public DateTime? UpdatedAtUtc { get; set; }
+
     public DateTime? LastCompletedAtUtc { get; set; }
 
     public List<HabitTag> HabitTags { get; set; }
+
     public List<Tag> Tags { get; set; }
 }
 
@@ -37,6 +50,7 @@ public enum HabitStatus
 public sealed class Frequency
 {
     public FrequencyType Type { get; set; }
+
     public int TimesPerPeriod { get; set; }
 }
 
@@ -51,11 +65,13 @@ public enum FrequencyType
 public sealed class Target
 {
     public int Value { get; set; }
+
     public string Unit { get; set; }
 }
 
 public sealed class Milestone
 {
     public int Target { get; set; }
+
     public int Current { get; set; }
 }
