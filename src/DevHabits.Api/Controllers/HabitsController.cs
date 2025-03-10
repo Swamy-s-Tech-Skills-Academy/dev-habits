a1d510f1feb421fd75d73a682ed8bc3b7631c0d2
Viswanatha Swamy <vishipayyallore@yahoo.com>
2025-03-10 14:02:25 +0530

    Update HabitsController.cs

--- src/DevHabits.Api/Controllers/HabitsController.cs
+++ src/DevHabits.Api/Controllers/HabitsController.cs
@@ -63,112 +63,112 @@ public sealed class HabitsController(ApplicationDbContext dbContext) : Controlle
         return Ok(paginationResult);
     }
 
-    [HttpGet("{id}")]
-    public async Task<IActionResult> GetHabit(
-        string id,
-        string? fields,
-        DataShapingService dataShapingService)
-    {
-        if (!dataShapingService.Validate<HabitWithTagsDto>(fields))
-        {
-            return Problem(
-                statusCode: StatusCodes.Status400BadRequest,
-                detail: $"The provided data shaping fields aren't valid: '{fields}'");
-        }
+    //[HttpGet("{id}")]
+    //public async Task<IActionResult> GetHabit(
+    //    string id,
+    //    string? fields,
+    //    DataShapingService dataShapingService)
+    //{
+    //    if (!dataShapingService.Validate<HabitWithTagsDto>(fields))
+    //    {
+    //        return Problem(
+    //            statusCode: StatusCodes.Status400BadRequest,
+    //            detail: $"The provided data shaping fields aren't valid: '{fields}'");
+    //    }
 
-        HabitWithTagsDto? habit = await dbContext
-            .Habits
-            .Where(h => h.Id == id)
-            .Select(HabitQueries.ProjectToDtoWithTags())
-            .FirstOrDefaultAsync();
+    //    HabitWithTagsDto? habit = await dbContext
+    //        .Habits
+    //        .Where(h => h.Id == id)
+    //        .Select(HabitQueries.ProjectToDtoWithTags())
+    //        .FirstOrDefaultAsync();
 
-        if (habit is null)
-        {
-            return NotFound();
-        }
+    //    if (habit is null)
+    //    {
+    //        return NotFound();
+    //    }
 
-        ExpandoObject shapedHabitDto = dataShapingService.ShapeData(habit, fields);
+    //    ExpandoObject shapedHabitDto = dataShapingService.ShapeData(habit, fields);
 
-        return Ok(shapedHabitDto);
-    }
+    //    return Ok(shapedHabitDto);
+    //}
 
-    [HttpPost]
-    public async Task<ActionResult<HabitDto>> CreateHabit(
-        CreateHabitDto createHabitDto,
-        IValidator<CreateHabitDto> validator)
-    {
-        await validator.ValidateAndThrowAsync(createHabitDto);
+    //[HttpPost]
+    //public async Task<ActionResult<HabitDto>> CreateHabit(
+    //    CreateHabitDto createHabitDto,
+    //    IValidator<CreateHabitDto> validator)
+    //{
+    //    await validator.ValidateAndThrowAsync(createHabitDto);
 
-        Habit habit = createHabitDto.ToEntity();
+    //    Habit habit = createHabitDto.ToEntity();
 
-        dbContext.Habits.Add(habit);
+    //    dbContext.Habits.Add(habit);
 
-        await dbContext.SaveChangesAsync();
+    //    await dbContext.SaveChangesAsync();
 
-        HabitDto habitDto = habit.ToDto();
+    //    HabitDto habitDto = habit.ToDto();
 
-        return CreatedAtAction(nameof(GetHabit), new { id = habitDto.Id }, habitDto);
-    }
+    //    return CreatedAtAction(nameof(GetHabit), new { id = habitDto.Id }, habitDto);
+    //}
 
-    [HttpPut("{id}")]
-    public async Task<ActionResult> UpdateHabit(string id, UpdateHabitDto updateHabitDto)
-    {
-        Habit? habit = await dbContext.Habits.FirstOrDefaultAsync(h => h.Id == id);
+    //[HttpPut("{id}")]
+    //public async Task<ActionResult> UpdateHabit(string id, UpdateHabitDto updateHabitDto)
+    //{
+    //    Habit? habit = await dbContext.Habits.FirstOrDefaultAsync(h => h.Id == id);
 
-        if (habit is null)
-        {
-            return NotFound();
-        }
+    //    if (habit is null)
+    //    {
+    //        return NotFound();
+    //    }
 
-        habit.UpdateFromDto(updateHabitDto);
+    //    habit.UpdateFromDto(updateHabitDto);
 
-        await dbContext.SaveChangesAsync();
+    //    await dbContext.SaveChangesAsync();
 
-        return NoContent();
-    }
+    //    return NoContent();
+    //}
 
-    [HttpPatch("{id}")]
-    public async Task<ActionResult> PatchHabit(string id, JsonPatchDocument<HabitDto> patchDocument)
-    {
-        Habit? habit = await dbContext.Habits.FirstOrDefaultAsync(h => h.Id == id);
+    //[HttpPatch("{id}")]
+    //public async Task<ActionResult> PatchHabit(string id, JsonPatchDocument<HabitDto> patchDocument)
+    //{
+    //    Habit? habit = await dbContext.Habits.FirstOrDefaultAsync(h => h.Id == id);
 
-        if (habit is null)
-        {
-            return NotFound();
-        }
+    //    if (habit is null)
+    //    {
+    //        return NotFound();
+    //    }
 
-        HabitDto habitDto = habit.ToDto();
+    //    HabitDto habitDto = habit.ToDto();
 
-        patchDocument.ApplyTo(habitDto, ModelState);
+    //    patchDocument.ApplyTo(habitDto, ModelState);
 
-        if (!TryValidateModel(habitDto))
-        {
-            return ValidationProblem(ModelState);
-        }
+    //    if (!TryValidateModel(habitDto))
+    //    {
+    //        return ValidationProblem(ModelState);
+    //    }
 
-        habit.Name = habitDto.Name;
-        habit.Description = habitDto.Description;
-        habit.UpdatedAtUtc = DateTime.UtcNow;
+    //    habit.Name = habitDto.Name;
+    //    habit.Description = habitDto.Description;
+    //    habit.UpdatedAtUtc = DateTime.UtcNow;
 
-        await dbContext.SaveChangesAsync();
+    //    await dbContext.SaveChangesAsync();
 
-        return NoContent();
-    }
+    //    return NoContent();
+    //}
 
-    [HttpDelete("{id}")]
-    public async Task<ActionResult> DeleteHabit(string id)
-    {
-        Habit? habit = await dbContext.Habits.FirstOrDefaultAsync(h => h.Id == id);
+    //[HttpDelete("{id}")]
+    //public async Task<ActionResult> DeleteHabit(string id)
+    //{
+    //    Habit? habit = await dbContext.Habits.FirstOrDefaultAsync(h => h.Id == id);
 
-        if (habit is null)
-        {
-            return NotFound();
-        }
+    //    if (habit is null)
+    //    {
+    //        return NotFound();
+    //    }
 
-        dbContext.Habits.Remove(habit);
+    //    dbContext.Habits.Remove(habit);
 
-        await dbContext.SaveChangesAsync();
+    //    await dbContext.SaveChangesAsync();
 
-        return NoContent();
-    }
+    //    return NoContent();
+    //}
 }
