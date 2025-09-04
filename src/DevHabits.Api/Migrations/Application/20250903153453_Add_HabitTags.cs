0428c2d9769dd32d4f6bc816295282fb432500b0
Viswanatha Swamy <vishipayyallore@yahoo.com>
2025-09-04 08:39:11 +0530

    Update EF migrations and add HabitTags migration (#19)

---
+++ src/DevHabits.Api/Migrations/Application/20250903153453_Add_HabitTags.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DevHabits.Api.Migrations.Application;
+
+/// <inheritdoc />
+public partial class Add_HabitTags : Migration
+{
+    /// <inheritdoc />
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+
+    }
+
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        // Method intentionally left empty.
+    }
+}
